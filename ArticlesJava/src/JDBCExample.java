a7474214d49dc054570acd1e94c040bc0a1c4eee
edoardo429 <76396911+edoardo429@users.noreply.github.com>
2021-06-17 02:32:58 +0200

    Add files via upload

---
+++ ArticlesJava/src/JDBCExample.java
@@ -0,0 +1,53 @@
+import java.sql.*;
+
+public class JDBCExample {
+
+	public static void main(String[] args) throws SQLException {
+
+		String databaseUsername = "root";
+
+		String databasePassword = "EdoSocial2007";
+
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/flights", databaseUsername,
+				databasePassword);
+
+		System.out.println("connection established successfully");
+
+		String selectQuery = "SELECT * FROM passenger";
+
+		Statement statement = connection.createStatement();
+
+		ResultSet resultSet = statement.executeQuery(selectQuery);
+
+		while (resultSet.next()) {
+
+			int passengerId = resultSet.getInt("passengerid");
+
+			String passengerName = resultSet.getString("passengername");
+
+			String passengerOrigin = resultSet.getString("passengerorigin");
+
+			String passengerDestination = resultSet.getString("passengerdestination");
+
+			System.out.println("\nPassenger Id: " + passengerId);
+
+			System.out.println("Passenger Name: " + passengerName);
+
+			System.out.println("Passenger Origin: " + passengerOrigin);
+
+			System.out.println("Passenger Destination: " + passengerDestination);
+
+		}
+
+		String insertQuery = "INSERT INTO passenger (passengername, passengerorigin, passengerdestination) VALUES  (?, ?, ?)";
+
+		PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+		preparedStatement.setString(1, "Martina Garcià");
+		preparedStatement.setString(2, "Valencia, Spain");
+		preparedStatement.setString(3, "Lyon, France"); 
+
+		preparedStatement.execute();
+
+	}
+
+}
